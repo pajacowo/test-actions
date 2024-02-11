025043dde6f55f15764990ef13518eadb773311d
pajacowo <github@pajacowo.pl>
2024-02-11 15:41:56 +0100

    yml 31

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test github automation v4
+          Test github automation v5
         </p>
         <a
           className="App-link"
